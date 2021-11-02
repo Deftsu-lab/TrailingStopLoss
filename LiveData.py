b2400ede740df1e459b291011c9d516ec79f295c
BenderDähnung <benlucabehring@gmail.com>
2021-11-02 18:28:20 +0100

    fix

--- LiveData.py
+++ LiveData.py
@@ -24,6 +24,15 @@ bsm = BinanceSocketManager(client)
 socket = bsm.trade_socket(pair)
 engine = sqlalchemy.create_engine(tableUrl)
 
+def delete_old(db):
+     conn = sqlite3.connect(db)
+     c = conn.cursor()
+     c.execute('DELETE FROM btcusdt;',)
+     print('Entferne ', c.rowcount, ' Zeilen an alten Daten')
+     conn.commit()
+     conn.close()
+     print('Sammle Livedaten...')
+
 def createframe(msg):
     df = pd.DataFrame([msg])
     df = df.loc[:,['s', 'E', 'p', 'E']]
@@ -41,14 +50,9 @@ async def main():
             if res:
                 frame = createframe(res)
                 frame.to_sql(pair, engine, if_exists='append', index=False)
-            if float(frame.Timestamp) % 3600 == 0:
-                conn = sqlite3.connect(db)
-                c = conn.cursor()
-                c.execute('DELETE FROM btcusdt;',)
-                print('Entferne ', c.rowcount, ' Zeilen an alten Daten')
-                conn.commit()
-                conn.close()
-                print('Sammle Livedaten...')
+                if float(frame.Timestamp) % 3600 == 0:
+                    delete_old(db)
+                   
     await client.close_connection()
 
 if __name__ == "__main__":
